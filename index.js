04b6d9658a34cabc5095e17c01465fcac2d3c16f
David Mongeau-Petitpas <dmongeau_petitpas@ecoutez.ca>
2013-01-07 21:24:40 -0500

    Bugfix when not providing repository name

--- index.js
+++ index.js
@@ -6,12 +6,14 @@ var path = require('path'),
 module.exports = Generator;
 
 function Generator() {
-    yeoman.generators.NamedBase.apply(this, arguments);
+    yeoman.generators.Base.apply(this, arguments);
 }
 
 util.inherits(Generator, yeoman.generators.NamedBase);
 
 Generator.prototype.fetchGit = function(name) {
+
+    if(!name || !name.length) name = 'basic';
 
     var cb = this.async();
     this.remote('folkloreatelier', 'yeoman-boilerplate-'+name, 'master', function(err, remote) {
